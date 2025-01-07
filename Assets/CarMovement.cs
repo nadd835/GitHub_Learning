3be0fa5a7b567895309a9c99ab10ddc82d828f09
nadd835 <nadirahamidah@gmail.com>
2025-01-07 10:24:50 +0700

    add car

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float speed = 10f; 
+    private float moveDuration = 5f; 
+    private float elapsedTime = 0f; 
+    private bool isMoving = true; 
+
+    void Update()
+    {
+
+        if (isMoving)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Debug.Log("Car is moving. Elapsed time: " + elapsedTime);
+            elapsedTime += Time.deltaTime;
+
+            // Stop moving after the duration has passed
+            if (elapsedTime >= moveDuration)
+            {
+                isMoving = false;
+                Debug.Log("Car stopped moving after " + elapsedTime + " seconds.");
+            }
+        }
+    }
+}
